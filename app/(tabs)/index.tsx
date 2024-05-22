57b03ee4330d205bd979b6c704cebbf47d2cb60c
soufiane chalouh <soufianechalouh@gmail.com>
2024-05-22 13:42:29 +0100

    Added moviePoster component

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,18 +1,20 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, FlatList } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
+import MovieImage from "@/components/MoviePoster";
+import categories from "@/assets/data/categories";
+
+const firstCategory = categories.items[0];
 
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
+      <Text style={styles.title}>Popular on Netflix</Text>
+      <FlatList
+        data={firstCategory.movies}
+        renderItem={({ item }) => <MovieImage path={item.poster} />}
+        horizontal
       />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
@@ -20,16 +22,9 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
   title: {
     fontSize: 20,
     fontWeight: "bold",
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
   },
 });
