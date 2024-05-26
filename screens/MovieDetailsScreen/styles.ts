b2f38e1d0dfffb497e8ec099d5c832d41cc16bfe
soufianechalouh <32603792+soufianechalouh@users.noreply.github.com>
2024-05-26 13:01:05 +0100

    Added details to title details screen (#7)
    * Added movie details screen
    
    * Added description for Movie Details screen
    
    * Moved style to styles file

---
+++ screens/MovieDetailsScreen/styles.ts
@@ -0,0 +1,75 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black", // Assuming you want a black background
+  },
+  image: {
+    width: "100%",
+    aspectRatio: 16 / 9,
+    resizeMode: "cover",
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  match: {
+    color: "#59d467",
+    fontWeight: "bold",
+    marginRight: 10,
+  },
+  year: {
+    color: "#757575",
+    marginRight: 5,
+  },
+  ageContainer: {
+    backgroundColor: "#e6e229",
+    borderRadius: 4,
+    paddingHorizontal: 3,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 5,
+  },
+  age: {
+    color: "black",
+    fontWeight: "bold",
+  },
+  playButton: {
+    backgroundColor: "white",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 3,
+    marginVertical: 5,
+  },
+  playButtonText: {
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  downloadButton: {
+    backgroundColor: "#2b2b2b",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 5,
+    borderRadius: 3,
+    marginVertical: 5,
+  },
+  downloadButtonText: {
+    color: "white",
+  },
+  plot: {
+    color: "white",
+    marginVertical: 10,
+  },
+  iconText: {
+    color: "darkgrey",
+    marginTop: 5,
+  },
+  iconContainer: {
+    alignItems: "center",
+    marginHorizontal: 20 },
+});
+
+export default styles;
