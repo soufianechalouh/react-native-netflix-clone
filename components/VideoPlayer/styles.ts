26b03fc28dcbf29a856fab8066b8eb8833f14e9d
soufiane chalouh <soufianechalouh@gmail.com>
2024-05-28 12:12:24 +0100

    removed unused styles

--- components/VideoPlayer/styles.ts
+++ components/VideoPlayer/styles.ts
@@ -1,20 +1,10 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    backgroundColor: "#ecf0f1",
-  },
   video: {
     alignSelf: "center",
     width: "100%",
     height: 200,
-  },
-  buttons: {
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
 
